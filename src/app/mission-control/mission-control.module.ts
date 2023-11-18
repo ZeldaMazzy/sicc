9b1726caf42ffd4a0efb668519e6d5f31f5816f6
ZeldaMazzy <69434725+ZeldaMazzy@users.noreply.github.com>
2023-11-18 12:28:34 -0600

    added the new feature module (#2)

---
+++ src/app/mission-control/mission-control.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MissionControlRoutingModule } from './mission-control-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MissionControlRoutingModule
+  ]
+})
+export class MissionControlModule { }
