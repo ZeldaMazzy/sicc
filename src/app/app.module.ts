5b6217e53b2ce6bd968e4b218a54712d7f427f96
mrkietzer <mrkietzer@gmail.com>
2023-11-18 13:10:18 -0600

    Updated Folder, Changed Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MissionColumnsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/mission-control/mission-control.module.ts
+++ src/app/mission-control/mission-control.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MissionControlRoutingModule } from './mission-control-routing.module';
+import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MissionColumnsComponent],
   imports: [
     CommonModule,
     MissionControlRoutingModule
