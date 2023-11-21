31d8b4dc15e608a50727b9f7e7422a54c5d8c929
MxMatRam <MxMatRam@pm.me>
2023-11-20 19:02:17 -0600

    declare MissionControlComponent, delete Missions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { MissionControlComponent } from './app/mission-control/mission-control.component';
 
 @NgModule({
   declarations:
