637b13c27de3b193ff067fc231f8ee004d22b84b
MxMatRam <MxMatRam@pm.me>
2023-11-20 19:28:09 -0600

    decl MCComp in M-C.mod, del Missions folder

--- src/app/mission-control/mission-control.module.ts
+++ src/app/mission-control/mission-control.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { MissionControlRoutingModule } from './mission-control-routing.module';
 import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
+import { MissionControlComponent } from './mission-control.component';
 
 
 @NgModule({
-  declarations: [MissionColumnsComponent],
+  declarations: [MissionControlComponent,
+    MissionColumnsComponent],
   imports: [
     CommonModule,
     MissionControlRoutingModule
