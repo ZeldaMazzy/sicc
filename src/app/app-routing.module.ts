4955dc9ae5382920b4dc17f69449676c2c015ba8
MxMatRam <MxMatRam@pm.me>
2023-11-20 18:42:12 -0600

    create missions mod w lazy-load in app rout mod

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'missions',
+loadChildren: () => import('./missions/missions.module')
+.then(m => m.MissionsModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/missions/missions.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MissionsRoutingModule } from './missions-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MissionsRoutingModule
+  ]
+})
+export class MissionsModule { }
