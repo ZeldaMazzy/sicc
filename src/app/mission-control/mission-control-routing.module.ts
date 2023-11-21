a720980e49896b008e6722d648d0821571b09fa4
MxMatRam <140029940+MxMatRam@users.noreply.github.com>
2023-11-20 19:37:46 -0600

    Mission Control Container Scaffolding (#6) -MR (#13)
    * Mission Control Container Scaffolding (#6) -MR
    
    created Mission Control container component with top-level routing(?) in mission-control-routing file
    
    * create missions mod w lazy-load in app rout mod
    
    * declare MissionControlComponent, delete Missions
    
    * decl MCComp in M-C.mod, del Missions folder
    
    ---------
    
    Co-authored-by: ZeldaMazzy <69434725+ZeldaMazzy@users.noreply.github.com>

--- src/app/mission-control/mission-control-routing.module.ts
+++ src/app/mission-control/mission-control-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MissionControlComponent } from './mission-control.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: MissionControlComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/mission-control/mission-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mission-control',
+  templateUrl: './mission-control.component.html',
+  styleUrls: ['./mission-control.component.scss']
+})
+export class MissionControlComponent {
+
+}
