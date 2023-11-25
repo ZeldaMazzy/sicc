9c95d4cffeb0d302bd1203a040feb626839a825a
mrkietzer <mrkietzer@gmail.com>
2023-11-25 16:30:46 -0600

    Completed CRUD operations

--- src/app/mission-control/mission-control.service.ts
+++ src/app/mission-control/mission-control.service.ts
@@ -2,17 +2,7 @@ import { Injectable } from '@angular/core';
 import { Mission } from '../shared/models/mission';
 import { EMPTY, Observable, findIndex, of } from 'rxjs';
 import { TEST_DATA_MISSIONDATA } from '../shared/stub/mission-control.stub';
-/*
-Mission Control Service
 
-Fill out the mission control service with CRUD operations. Each operation should return an observable with test data, as we have not set up a backend yet.
-
-Here are the acceptance criteria:
-    Create an updateMission endpoint, which returns the updated Mission in an observable
-    Create an addNewMission endpoint, which returns an created Mission in an observable
-    Create a deleteMission endpoint, which returns an empty observable
-
-*/
 @Injectable({
   providedIn: 'root'
 })
@@ -31,9 +21,24 @@ export class MissionControlService {
     return of(mission || null);
   }
 
-  UpdateMission(mission: Mission): EMPTY {
-    const missionIndex: Mission.findIndex(mission => {
-
+  UpdateMission(missionToChange: Mission): Observable<Mission> {
+    const missionIndex: number = TEST_DATA_MISSIONDATA.findIndex(mission => {
+      return mission.MissionID === missionToChange.MissionID;
     })
+    TEST_DATA_MISSIONDATA[missionIndex] = missionToChange;
+    return of(missionToChange);
+  }
+
+  AddNewMission(addMission: Mission): Observable<Mission> {
+    TEST_DATA_MISSIONDATA.push();
+    return of(addMission)
+  }
+
+  RemoveMission(deleteMission: Mission): Observable<void> {
+    const missionIndex: number = TEST_DATA_MISSIONDATA.findIndex(mission => {
+      return mission.MissionID === deleteMission.MissionID;
+    })
+    TEST_DATA_MISSIONDATA.splice(missionIndex);
+    return EMPTY;
   }
 }
