233c76132828585acbe7ce5524661e3510d22c28
mrkietzer <131722826+mrkietzer@users.noreply.github.com>
2023-11-27 20:15:11 -0600

    Edit Task Scaffolding (#21)

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 const routes: Routes = [
-  {path:'', component:TasksComponent},
+  { path: '', component: TasksComponent },
+  { path: 'add-new-task', component: EditTaskComponent },
+  { path: 'add-new-task/:mission-id/:phase-id', component: EditTaskComponent },
+  { path: 'edit-task/:task-id', component: EditTaskComponent }
 ];
 
 @NgModule({
