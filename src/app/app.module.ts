4f5ec352093db2732ac12ad09b4959bea02d5704
Slingshot1942 <140355325+Slingshot1942@users.noreply.github.com>
2023-11-30 20:18:49 -0600

    Added Authentication Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations:
   [AppComponent],
