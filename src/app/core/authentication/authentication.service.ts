39683586c301c6a6e86730d15d20b741ffd7bde3
Slingshot1942 <140355325+Slingshot1942@users.noreply.github.com>
2023-12-04 18:07:32 -0600

    Added Authentication Service

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -1,10 +1,52 @@
 import { Injectable } from '@angular/core';
 import { AuthenticationComponent } from './authentication.component';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { AuthenticationResponse } from './authentication-response.model';
+import { User } from 'src/app/shared/user/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
 
-  constructor() { }
+export class AuthenticationService {
+  private readonly loginURL: string = environment.firebaseLoginURL + environment.API_KEY;
+  private readonly signupURL: string = environment.firebaseLoginURL + environment.API_KEY;
+  currentuser: any;
+
+
+constructor(private http: HttpClient, private router: Router) {}
+
+public signUp(email: string, password: string): Observable<AuthenticationResponse> {
+  const request: any = {
+    email, password,
+    returnSecureToken: true
+  }
+
+  return this.http.post<AuthenticationResponse>(this.signupURL, request)
+  .pipe(tap(authenticationResponse => this.handleAuth(authenticationResponse)));
 }
+
+public signOut(): void {
+  this.currentuser.next(null);
+  this.router.navigate(["authentication"]);
+}
+
+public getUserDataFromStorage(): AuthenticationResponse | undefined {
+  return JSON.parse(localStorage.getItem("UserData"));
+}
+
+public getAccessTokenFromStorage(): string {
+  if(!localStorage.getItem("userData")) return "";
+  const data = JSON.parse(localStorage.getItem("UserData"));
+  return data.accessToken;
+}
+
+public handleAuth(authResponse: AuthenticationResponse): void {
+  const {email, localId, idToken} = authResponse
+  const expiresIn: number = Date.now() + +authResponse.expiresIn;
+}
+}
+
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  firebaseRootURL: "https://sicc-9aaeb-default-rtdb.firebaseio.com/",
+  firebaseLoginURL: "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=",
+  firebaseSignupURL: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=",
+  API_KEY: "AIzaSyAi5NocHcM4CQ_gUA-MwMKNpfB2PhSUpDM"
+};
