2c001ae1b30bed9160d8af39dad99cb0e4db65f3
SkylarMiller <smiller5862@gmail.com>
2023-12-07 14:32:53 -0600

    missionlist scaffolding

--- src/app/mission-control/mission-control-routing.module.ts
+++ src/app/mission-control/mission-control-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MissionControlComponent } from './mission-control.component';
+import { MissionListComponent } from './mission-list/mission-list.component';
 
 const routes: Routes = [{
   path: ':mission-id',
   component: MissionControlComponent
-}
+},
+{path: 'mission-list',
+component: MissionListComponent}
 ];
 
 @NgModule({
--- src/app/mission-control/mission-control.module.ts
+++ src/app/mission-control/mission-control.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MissionControlRoutingModule } from './mission-control-routing.module';
 import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
 import { MissionControlComponent } from './mission-control.component';
+import { MissionListComponent } from './mission-list/mission-list.component';
 
 
 @NgModule({
   declarations: [MissionControlComponent,
-    MissionColumnsComponent],
+    MissionColumnsComponent,
+    MissionListComponent],
   imports: [
     CommonModule,
     MissionControlRoutingModule
