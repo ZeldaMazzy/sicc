254bdbe6fe01a2fa726298b2bfc34a3a159eb57c
SkylarMiller <smiller5862@gmail.com>
2023-12-07 14:42:38 -0600

    missioncard scaffolding

---
+++ src/app/mission-control/mission-card/mission-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Mission } from 'src/app/shared/models/mission';
+
+@Component({
+  selector: 'app-mission-card',
+  templateUrl: './mission-card.component.html',
+  styleUrls: ['./mission-card.component.scss']
+})
+export class MissionCardComponent {
+  @Input() mission!:Mission;
+}
--- src/app/mission-control/mission-control.module.ts
+++ src/app/mission-control/mission-control.module.ts
@@ -5,12 +5,14 @@ import { MissionControlRoutingModule } from './mission-control-routing.module';
 import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
 import { MissionControlComponent } from './mission-control.component';
 import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionCardComponent } from './mission-card/mission-card.component';
 
 
 @NgModule({
   declarations: [MissionControlComponent,
     MissionColumnsComponent,
-    MissionListComponent],
+    MissionListComponent,
+    MissionCardComponent],
   imports: [
     CommonModule,
     MissionControlRoutingModule
