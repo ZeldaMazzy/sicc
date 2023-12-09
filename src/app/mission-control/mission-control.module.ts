def6c4b49eb6c07f841c299fe362a6f85fa685c6
SkylarMiller <140355511+SkylarMiller@users.noreply.github.com>
2023-12-09 14:09:55 -0600

    Mission list card edit scaffolding sm (#34)
    * missionlist scaffolding
    
    * missioncard scaffolding
    
    * missionedit scaffolding

--- src/app/mission-control/mission-control.module.ts
+++ src/app/mission-control/mission-control.module.ts
@@ -4,11 +4,17 @@ import { CommonModule } from '@angular/common';
 import { MissionControlRoutingModule } from './mission-control-routing.module';
 import { MissionColumnsComponent } from './mission-columns/mission-columns.component';
 import { MissionControlComponent } from './mission-control.component';
+import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionCardComponent } from './mission-card/mission-card.component';
+import { MissionEditComponent } from './mission-edit/mission-edit.component';
 
 
 @NgModule({
   declarations: [MissionControlComponent,
-    MissionColumnsComponent],
+    MissionColumnsComponent,
+    MissionListComponent,
+    MissionCardComponent,
+    MissionEditComponent],
   imports: [
     CommonModule,
     MissionControlRoutingModule
