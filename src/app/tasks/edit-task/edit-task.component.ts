28779a4412bee5e85ff4e7f4c78332b6a94f0d79
SkylarMiller <smiller5862@gmail.com>
2023-12-16 21:23:35 -0600

    deleted spec file, injected tasksservice routerservice

--- src/app/tasks/edit-task/edit-task.component.ts
+++ src/app/tasks/edit-task/edit-task.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from 'src/app/shared/models/task';
+import { TasksService } from '../tasks.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-edit-task',
@@ -6,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-task.component.scss']
 })
 export class EditTaskComponent {
+task!: Task;
 
+constructor(
+  private tasksService: TasksService,
+  private routerService: Router,
+){}
 }
